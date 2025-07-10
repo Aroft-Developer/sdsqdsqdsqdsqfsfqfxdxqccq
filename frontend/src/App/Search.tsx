a097ac2df1e60c33f2e52e21b65885a62e227629
Aroft <139874241+Aroft-Developer@users.noreply.github.com>
2025-07-10 09:29:25 +0200

    Update Search.tsx

--- frontend/src/App/Search.tsx
+++ frontend/src/App/Search.tsx
@@ -60,7 +60,8 @@ function Search({ isDark }: SearchProps) {
     <div
       className={`font-[Outfit] w-full h-[calc(100vh-68px-68px)] flex flex-col items-center justify-center transition ${
         isDark ? "bg-white text-[#1d283a]" : "bg-[#040712] text-white"
-      }`}
+      }
+      pt-0 pb-0 sm:pt-0 sm:pb-0 max-sm:pt-6 max-sm:pb-6`}
     >
       <div className="items-center w-full max-w-[990px] px-4">
         {!hasResponse && !loading && (
